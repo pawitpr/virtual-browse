3da96460b5b9d47e5e3442c1267c06d1b19f78c2
pawitpr <pawitprogram@gmail.com>
2023-03-06 05:28:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const Browser = require('discordmium');
+const TOKEN = "MTA4MDk0NTMxMDY0NzQ1NTgxNA.GgZoF3.x6Gr2PWcSl0akZX6vpueX9raNMkZFM5C6aeLAU"
+const ID = "1070097592257626265"
+Browser(TOKEN, ID)
