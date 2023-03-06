0dfd2e502b2e16594baeccd862203d77ca720293
PawitSahare <123424956+pawitpr@users.noreply.github.com>
2023-03-06 05:35:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const Browser = require('discordmium');
-const TOKEN = "MTA4MDk0NTMxMDY0NzQ1NTgxNA.GgZoF3.x6Gr2PWcSl0akZX6vpueX9raNMkZFM5C6aeLAU"
+const TOKEN = ""
 const ID = "1070097592257626265"
 Browser(TOKEN, ID)
